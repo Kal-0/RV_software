a038c746b0df09dfb88eea64759bd0736bffa9f2
Kal <hiratelli.0@gmail.com>
2024-09-03 17:44:36 -0300

    init of spring project

---
+++ BloodLab/src/main/java/Cesar/Rv/BloodLab/BloodLabApplication.java
@@ -0,0 +1,13 @@
+package Cesar.Rv.BloodLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodLabApplication.class, args);
+	}
+
+}
---
+++ BloodLab/src/test/java/Cesar/Rv/BloodLab/BloodLabApplicationTests.java
@@ -0,0 +1,13 @@
+package Cesar.Rv.BloodLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
