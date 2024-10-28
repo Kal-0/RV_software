17cacdfdd4d182a158e838c4b8905c79ee4bb319
Kal-0 <106926790+Kal-0@users.noreply.github.com>
2024-10-28 15:25:51 -0300

    adding jpa persistance layer

--- ClinicalLab/ClinicalLab-Infrastructure/src/main/java/infrastructure/persistence/jpa/ClientJPA.java
+++ ClinicalLab/ClinicalLab-Infrastructure/src/main/java/infrastructure/persistence/jpa/ClientJPA.java
@@ -1,5 +1,36 @@
 package infrastructure.persistence.jpa;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "clients")
 public class ClientJPA {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String contactEmail;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    
 }
